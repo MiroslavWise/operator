86b67350b285ec51c0f2c8a898db3181ed8cfa3a
MiroslavWise <79138594+MiroslavWise@users.noreply.github.com>
2022-02-15 20:36:31 +0300

    Основной залив

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='navigation'>
+      <h1>Operator</h1>
+      <nav>
+          <Link to="mts"><button><img src='https://upload.wikimedia.org/wikipedia/commons/c/c2/MTS_logo.svg' height={'24px'} alt='MTS'/><a>MTS</a></button></Link>
+          <Link to="beeline"><button><img src={'https://upload.wikimedia.org/wikipedia/en/b/b1/Beeline.svg'} height={'24px'} alt='Beeline'/><a>Beeline</a></button></Link>
+          <Link to="megafon"><button><img src={'https://upload.wikimedia.org/wikipedia/commons/9/9e/MegaFon_logo.svg'}  width={'40px'}/><a>Megafon</a></button></Link>
+      </nav>
     </div>
   );
 }
